6a907de3f62d692a78070541f6c0ff993c77a71c
Gustavo <gustavoramon1999@gmail.com>
2022-05-30 11:46:17 -0500

    Se agrego el algoritmo 3 en JS

--- Algoritmos 3/js/main.js
+++ Algoritmos 3/js/main.js
@@ -1,11 +1,37 @@
 //3.	Realizar un algoritmo para adivinar un número entre el 1 y el 100 en el menor número de pasos posibles
 
-let = numPrim;
+
 
 alert("Adivinare tu Numero");
-for(let i = 1; i<= 10; i++){
-    numPrim = prompt("¿El Numero en que pinesas es numero primo de "+ i + "?");
+alert("Escribe 1 para Si, 2 para No");
 
+for(let i = 1; i<= 10; i++){
+    numPrim =  prompt("¿El Numero en que pinesas es numero primo de "+ i + "?");
+    if(numPrim == 1){
+        break;
+    }
+    console.log("Numero Primo es "+ numPrim);
+    console.log("Numero Ciclo e "+ i);
+    numFinUser = i + 1;
+    console.log("Numero Primo Final es "+ numFinUser);
+}
+
+numFinUser = numFinUser;
+
+for(let l = 1; l<= 100; l++){
+    numeroT = numFinUser * l;4
+    console.log("Segundo Numero Final For: "+ numeroT);
+    numPrimF = prompt("¿Tu numero es "+ numeroT);
+    console.log("Numero a Multiplicar por el Primo" + numeroT);
+    if(numPrimF == 1){
+        alert("Lo adivine XD");
+        break;
+    }
+    if(numeroT >= 100){
+        alert("¿ENTONCES CUAL ES? XD ");
+        break;
+    }
+    
 }
 
 
